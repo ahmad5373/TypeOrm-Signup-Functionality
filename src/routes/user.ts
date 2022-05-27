5c1ddfd6b868532cc22ad944bddb56db5f91fb89
Ahmed Raza <ahmadraxa550@gmail.com>
2022-05-27 02:32:34 -0700

    Signup validation in user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,12 @@
 import * as express from "express";
 import { Router } from "express";
+import { SignupRequest } from "src/request/auth";
 
 import {createUser ,login} from "../controller/user_controller";
 
 const router: Router = express.Router();
 
-router.post("/signup" , createUser);
+router.post("/signup" , SignupRequest, createUser);
 router.post("/login" , login);
 
 export default router;
